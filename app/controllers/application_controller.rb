ac72fb636096184fccba384ddc90d420d99f98e7
jared frankel <jaredfrankel10@gmail.com>
2018-02-05 20:22:19 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,7 @@ before_action :authenticate_user
   private
 
     def authenticate_user
-      client_id = ENV['GITHUB_CLIENT_ID']
-       github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&scope=repo"
-       redirect_to github_url unless logged_in?
+      redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&scope=repo" if !logged_in?
     end
 
     def logged_in?
